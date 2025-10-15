7fa88da5c7b08d2184a637e404da648fdcfe7aef
Mia <mia@mia.cx>
2025-10-15 16:02:37 +0200

    turn off page titles in transcluded embeds (this should be a configurable boolean somewhere)

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -143,21 +143,21 @@ function renderTranscludes(root: Root, cfg: GlobalConfiguration, slug: FullSlug,
                 } else if (page.htmlAst) {
                     // page transclude
                     node.children = [
-                        {
-                            type: "element",
-                            tagName: "h1",
-                            properties: {},
-                            children: [
-                                {
-                                    type: "text",
-                                    value:
-                                        page.frontmatter?.title ??
-                                        i18n(cfg.locale).components.transcludes.transcludeOf({
-                                            targetSlug: page.slug!,
-                                        }),
-                                },
-                            ],
-                        },
+                        // {
+                        //     type: "element",
+                        //     tagName: "h1",
+                        //     properties: {},
+                        //     children: [
+                        //         {
+                        //             type: "text",
+                        //             value:
+                        //                 page.frontmatter?.title ??
+                        //                 i18n(cfg.locale).components.transcludes.transcludeOf({
+                        //                     targetSlug: page.slug!,
+                        //                 }),
+                        //         },
+                        //     ],
+                        // },
                         ...(page.htmlAst.children as ElementContent[]).map((child) =>
                             normalizeHastElement(child as Element, slug, transcludeTarget),
                         ),
